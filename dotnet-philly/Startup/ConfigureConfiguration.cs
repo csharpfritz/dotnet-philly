fe875fbabd15c74f7868fe07da6127b5f7ccf470
Anthony Conrad <anthony@parithon.com>
2018-06-28 11:22:01 -0700

    Now using DI to configure services and introduced configurations.

---
+++ dotnet-philly/Startup/ConfigureConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_philly.Startup
+{
+    internal static class ConfigureConfiguration
+    {
+        internal static IConfiguration Execute()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DotNet_Environment")}.json", true, true)
+                // ApplicationData results in the following paths:
+                // Windows: <UserProfile>/AppData/Roaming
+                // Mac:     /Users/<user>/.config
+                // Linux:   /home/<user>/.config
+                .AddJsonFile($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/dotnet-philly/appsettings.json", true, true)
+                .Build();
+        }
+    }
+}
